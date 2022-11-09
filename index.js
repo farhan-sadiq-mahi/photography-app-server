dff76cf383b80010732e5cf0f7a05465344d8518
farhan-sadiq-mahi <fsmahi100@gmail.com>
2022-11-10 01:13:06 +0600

    all service api created

--- index.js
+++ index.js
@@ -77,6 +77,26 @@ app.get('/service/:id', async (req, res) => {
 })
 
 
+app.get('/services', async (req, res) => {
+    try {
+        const query = {};
+        const cursor = servicesCollection.find(query);
+        const result = await cursor.toArray();
+        res.send({
+            success: true,
+            message: 'Successfully got the services data',
+            data: result
+        })
+    } catch (error) {
+        console.log(error.name, error.message);
+        res.send({
+            success: false,
+            message: error.message
+        })
+
+    }
+})
+
 
 app.post('/addservice', async (req, res) => {
     try {
