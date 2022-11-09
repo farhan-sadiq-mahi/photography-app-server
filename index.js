7f34a6ba3b693024d37d2c43cf8b9baf1df3923f
farhan-sadiq-mahi <fsmahi100@gmail.com>
2022-11-10 00:29:21 +0600

    home service data added

--- index.js
+++ index.js
@@ -7,16 +7,76 @@ require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
+console.log()
 
-
-const uri = "mongodb+srv://process.env.DB_USER:process.env.DB_PASS@cluster0.gaubth5.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB}:${process.env.DB_PASS}@cluster0.gaubth5.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function dbConnect() {
+    try {
+        await client.connect();
+        console.log('database connected');
+    } catch (error) {
+        console.log(error.name, error.message)
+    }
+}
+dbConnect();
+
+
+const servicesCollection = client.db("MyPhotography").collection("services");
 
 
 
 app.get('/', (req, res) => {
     res.send('hello form server')
+})
+
+
+
+app.get('/homeservices', async (req, res) => {
+    try {
+        const query = {};
+        const cursor = servicesCollection.find(query).sort({ "dateTime": -1 }).limit(3);
+        const result = await cursor.toArray();
+        res.send({
+            success: true,
+            message: 'Got The Data',
+            data: result
+        })
+    } catch (error) {
+        console.log(error.name, error.message);
+        res.send({
+            success: false,
+            message: error.message
+        })
+
+    }
+})
+
+
+
+app.post('/addservice', async (req, res) => {
+    try {
+        const service = req.body;
+        const result = await servicesCollection.insertOne(service)
+        if (result.insertedId) {
+            res.send({
+                success: true,
+                message: 'Successfully added the Service'
+            })
+        }
+        else {
+            res.send({
+                success: false,
+                message: 'Cannot add the Service'
+            })
+        }
+    } catch (error) {
+
+    }
+
+
+
 })
 
 
